800efe8666dd4169b82d73920e3030b194f4faa6
eonianmonk <no.mourner@gmail.com>
2024-01-12 21:34:55 +0200

    changed cli args to flags

--- cmd/commands.go
+++ cmd/commands.go
@@ -3,9 +3,9 @@ package main
 import "github.com/alecthomas/kingpin"
 
 var (
-	app = kingpin.New("hc", "health checker")
+	app = kingpin.New("app", "health checker")
 	runCmd = app.Command("run", "run service")
 	stopOnFail = runCmd.Flag("stopIfFailed","set to stop ping process if one of url respond with fail").Bool()
-	port = runCmd.Arg("port","port to start listening").Default(":8080").String()
-	maxTimeout = runCmd.Arg("timeout","set maximum timeout for requests").Default("5").Int()
+	port = runCmd.Flag("port","port to start listening").Default(":8080").String()
+	maxTimeout = runCmd.Flag("timeout","set maximum timeout for requests").Default("5").Int()
 )
