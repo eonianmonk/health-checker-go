b89ff61721f0c73f5b74895400886762d779fc34
eonianmonk <no.mourner@gmail.com>
2024-01-12 21:20:48 +0200

    fixed cls logic

--- cmd/commands.go
+++ cmd/commands.go
@@ -5,7 +5,7 @@ import "github.com/alecthomas/kingpin"
 var (
 	app = kingpin.New("hc", "health checker")
 	runCmd = app.Command("run", "run service")
-	stopOnFail = app.Flag("stopIfFailed","set to stop ping process if one of url respond with fail").Bool()
-	port = app.Arg("port","port to start listening").Default(":8080").String()
-	maxTimeout = app.Arg("timeout","set maximum timeout for requests").Default("5").Int()
+	stopOnFail = runCmd.Flag("stopIfFailed","set to stop ping process if one of url respond with fail").Bool()
+	port = runCmd.Arg("port","port to start listening").Default(":8080").String()
+	maxTimeout = runCmd.Arg("timeout","set maximum timeout for requests").Default("5").Int()
 )
